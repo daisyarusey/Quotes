6ae275b89c92490b5b17e44258d458024ebe3056
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-16 16:45:13 +0300

    nest the goal component inside the app component, using quote component's selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+  
   ],
   imports: [
     BrowserModule,
