e111120057eb974682f0fbe18b40d4837155688f
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-19 01:23:28 +0300

    create the submitQuote() function in which we use the emit method and pass in the new goal object we want to create.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -15,6 +15,10 @@ export class QuoteFormComponent implements OnInit {
 
   @Output() addQuote = new EventEmitter<Quote>();
 
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
+
   constructor() { }
 
   ngOnInit() {
