9105538e8a2ea636ea671c8573be3c39cc5f2e30
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-19 01:56:21 +0300

    Add the date attribute

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -11,12 +11,13 @@ import { from } from 'rxjs';
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuote=new Quote(0,"","","");
+  newQuote=new Quote(0,"","","",new Date());
 
   @Output() addQuote = new EventEmitter<Quote>();
 
   submitQuote(){
     this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0,"","","",new Date())
   }
 
   constructor() { }
